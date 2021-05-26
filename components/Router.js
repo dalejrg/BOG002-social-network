26e9e517a19626669b90d7c9570a68007a91deee
dalejrg <74320395+dalejrg@users.noreply.github.com>
2021-05-26 12:13:22 -0500

    Updates

--- components/Router.js
+++ components/Router.js
@@ -1,47 +1,54 @@
 import { Loading } from "./Loading.js";
 import { Landing, random } from "./Landing.js";
 import { Register, addUser } from "./Register.js";
-import { Home, menu, renderPosts } from "./Home.js";
+import { Home, menu, userOut } from "./Home.js";
 import { Login, logInUser } from "./Login.js";
-import { Post, menuHam, postFb } from "./Post.js";
+import { Post, menuHam, postFb, userOutPost } from "./Post.js";
 import { Password, recoverPassword } from "./Password.js";
+import { getPost } from "../Firebase/Storage.js";
+import { editProfile } from "./editProfile.js";
 
 export function Router() {
-  const { hash } = location;
-  const root = document.querySelector("#template");
-  switch (hash) {
-    case "":
-    case "#/":
-      root.appendChild(Loading());
-      break;
-    case "#/landing":
-      root.appendChild(Landing());
-      random();
-      break;
-    case "#/register":
-      root.appendChild(Register());
-      addUser();
-      break;
-    case "#/login":
-      root.appendChild(Login());
-      logInUser();
-      break;
-    case "#/home":
-      root.appendChild(Home());
-      renderPosts();
-      // userOut();
-      menu();
-      break;
-    case "#/post":
-      root.appendChild(Post());
-      menuHam();
-      postFb();
-      break;
-    default:
-      break;
-    case "#/password":
-      root.appendChild(Password());
-      recoverPassword();
-      break;
-  }
+    const { hash } = location;
+    const root = document.querySelector("#template");
+    switch (hash) {
+        case "":
+        case "#/":
+            root.appendChild(Loading());
+            break;
+        case "#/landing":
+            root.appendChild(Landing());
+            random();
+            break;
+        case "#/register":
+            root.appendChild(Register());
+            addUser();
+            break;
+        case "#/login":
+            root.appendChild(Login());
+            logInUser();
+            break;
+        case "#/home":
+            root.appendChild(Home());
+            getPost();
+            userOut();
+            menu();
+            break;
+        case "#/post":
+            root.appendChild(Post());
+            menuHam();
+            postFb();
+            userOutPost();
+            break;
+        case "#/editprofile":
+            root.appendChild(editProfile());
+            menuHam();
+            break;
+        default:
+            break;
+        case "#/password":
+            root.appendChild(Password());
+            recoverPassword();
+            break;
+    }
 }
--- components/Utils.js
+++ components/Utils.js
@@ -1,41 +1,87 @@
+import { deleteObjPost } from "../Firebase/Storage.js"
+
 export function reset() {
-  const template = document.querySelector("#template");
-  if (template) {
-    template.innerHTML = "";
-  }
+    const template = document.querySelector("#template");
+    if (template) {
+        template.innerHTML = "";
+    }
 }
 
+export function cleanPost() {
+    const containerPost = document.querySelector("#render");
+    if (containerPost) {
+        containerPost.innerHTML = "";
+    }
+}
+
+
+
 export function showPassword(input, show, hide) {
-  input.type = ("type", "text");
-  show.style.display = "none";
-  hide.style.display = "block";
+    input.type = ("type", "text");
+    show.style.display = "none";
+    hide.style.display = "block";
 }
 
 export function hidePassword(input, hide, show) {
-  input.type = ("type", "password");
-  hide.style.display = "none";
-  show.style.display = "block";
+    input.type = ("type", "password");
+    hide.style.display = "none";
+    show.style.display = "block";
 }
 
 export function homeListener(menuIcon) {
-  menuIcon.classList.toggle("open");
+    menuIcon.classList.toggle("open");
 }
 
 export function menuPrincipal(navegator) {
-  navegator.classList.toggle("is_active");
+    navegator.classList.toggle("is_active");
 }
 
 export function renderPost(doc) {
-  const containerPost = document.querySelector("#render");
-  const previewHTML = containerPost.innerHTML;
+    const containerPost = document.querySelector("#render");
+    const previewHTML = containerPost.innerHTML;
+    containerPost.innerHTML = `
+<div class="newPost">
+    <div class="headerPost">
 
-  containerPost.innerHTML = `
-  <div class="newPost">
-      <div class="user"></div>
-      <div><img class="imgPost" src= ${doc.image}></div>
-      <div class="text"><p class"textPost">${doc.description}</p></div>
-  </div>
-  ${previewHTML}
-  `;
-  //return containerPost
+    <div class="user">
+    <div class="name">Username</div>
+    <div class="timePost">${new Date().toLocaleDateString('es-CO')}</div>
+    </div>
+
+    <div class="options">
+    <button id="dots"><img class="btn-options" src=./assets/btn-options.svg alt="options"></button>
+    </div>
+
+    <nav class="optionPost">
+    <ul>
+    <li><button id="editPost">Edit</button></li>
+    <li><button id="deletePost-${doc.id}">Delete</button></li>
+    </ul>
+    </nav>
+
+    </div>
+    
+    <div><img class="imgPost" src= ${doc.image}></div>
+    <div class="socialPost">
+    <div><img class="like" src=./assets/like.svg alt="like"></div>
+    <div><img class="share" src=./assets/share.svg alt="share"></div>
+    </div>
+    <div><p class="textPost">${doc.description}</p></div>
+    <div><input type="text" class="comment" placeholder="Write a comment"/></div>
+</div>
+<!-- Es una prueba -->
+${previewHTML}
+`;
+    /*const btnDots = document.querySelector('#dots')
+    btnDots.addEventListener("click", () => {
+    });*/
+
+    const edit = document.querySelector('#editPost')
+    edit.addEventListener("click", () => {});
+
+    const deletePost = document.querySelector(`#deletePost-${doc.id}`)
+    console.log(deletePost)
+    deletePost.addEventListener("click", () => {
+        deleteObjPost(`${doc.id}`);
+    })
 }
