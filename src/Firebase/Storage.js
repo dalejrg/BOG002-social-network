36d5fbcad6ab0e641d28572894c87a414a18415c
hohjanna <ardilayoki@gmail.com>
2021-05-31 11:27:36 -0500

    Liked botton

--- src/Firebase/Storage.js
+++ src/Firebase/Storage.js
@@ -4,25 +4,25 @@ import { renderPost, cleanPost } from "../components/Utils.js"
 const db = firebase.firestore();
 export function savePost(file, description, idUser, name) {
     uploadImage(file).then((url) => {
-        db.collection("usersPost").doc().set({
+        db.collection('usersPost').doc().set({
             image: url,
             description,
             date: firebase.firestore.FieldValue.serverTimestamp(),
             name,
             idUser,
+            likesCount: 0,
         });
     });
 }
 
 export function getPost() {
-    db.collection("usersPost").orderBy('date', 'asc').onSnapshot(query => {
+    db.collection('usersPost').orderBy('date', 'asc').onSnapshot(query => {
         let changePost = query.docs;
+        console.log(changePost)
         const timeline = [];
         cleanPost()
         changePost.forEach(post => {
-            console.log(post)
             timeline.push(post.data())
-                //console.log({ id: post.doc.id, ...post.doc.data(), date: new Date() })
             renderPost({ id: post.id, ...post.data(), })
         })
     })
@@ -30,7 +30,7 @@ export function getPost() {
 
 function uploadImage(file) {
     const name = file.name || "";
-    const ref = firebase.storage().ref("/userProfileImgs/" + name);
+    const ref = firebase.storage().ref('/userProfileImgs/' + name);
     const uploadTask = ref.put(file);
     return uploadTask
         .then((snapshot) => snapshot.ref.getDownloadURL())
@@ -39,32 +39,34 @@ function uploadImage(file) {
         });
 }
 
-
-
 export function deleteObjPost(idPost) {
-    db.collection("usersPost").doc(idPost).delete().then(() => {
-        console.log("Document successfully deleted!");
+    db.collection('usersPost').doc(idPost).delete().then(() => {
+        console.log('Document successfully deleted!');
     }).catch((error) => {
-        console.error("Error removing document: ", error);
+        console.error('Error removing document: ', error);
     });
 }
 
-export function updateObjPost (id, description) {
-    const updatePost = db.collection("usersPost").doc(id);
-    const editar = document.querySelector(".inputPost").value;
+export function updateObjPost(idPost, newText) {
+    const updatePost = db.collection('usersPost').doc(idPost);
     return updatePost.update({
-        description: editar,
+            description: newText,
         })
         .then(() => {
-            console.log("Document successfully update!");
+            console.log('Document successfully update!');
         }).catch((error) => {
-            console.error("Error update document: ", error);
+            console.error('Error update document: ', error);
         });
 }
-    
 
-
-
-
-// Set the "capital" field of the city 'DC'
-
+export function likeObjPost(idPost, incrementer) {
+    const postLikeUsers = db.collection('usersPost').doc(idPost);
+    return postLikeUsers.update({
+            likesCount: incrementer,
+        })
+        .then(() => {
+            console.log('Document successfully liked!');
+        }).catch((error) => {
+            console.error('Error liked document: ', error);
+        });
+}
